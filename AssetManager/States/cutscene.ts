6412726ee8623f485afb336d88cf1f4fafdd8135
PLyn <plyn10@stu.ncu.edu.jm>
2014-03-06 20:06:58 -0500

    added TODO for use later

--- AssetManager/States/cutscene.ts
+++ AssetManager/States/cutscene.ts
@@ -1,8 +1,12 @@
 ﻿///<reference path='State.ts' />
 module Game {
     export class Cutscene extends State {
+        dia;
         constructor() {
             super();
+            //create new canvas to put dialogue on
+            this.dia = new Dialogue(ctx, w);
+            this.dia.startScene('chapter', 'scene', 0);
         }
         update() {
 
