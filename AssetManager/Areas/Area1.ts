35daed2f4946694d4022b83e7849872c515e53f3
PLyn <plyn10@stu.ncu.edu.jm>
2014-03-13 01:17:40 -0500

    added left out files

---
+++ AssetManager/Areas/Area1.ts
@@ -0,0 +1,17 @@
+﻿///<reference path='genericArea.ts' />
+module Game {
+    export class Area1 extends GenericArea {
+        stateManger;
+        constructor(ctx, w) {
+            super(ctx, w)
+            this.stateManger = new StateManager();
+            sManager.pushState(new Explore(ctx, w, 'rpg', this));
+        }
+        update = () => {
+            sManager.updateStack();
+        }
+        nextState(objectID) {
+            sManager.popState();
+        }
+    }
+}
---
+++ AssetManager/Areas/Area2.ts
@@ -0,0 +1,19 @@
+﻿///<reference path='genericArea.ts' />
+module Game {
+    export class Area2 extends GenericArea {
+        stateManger;
+        ctx;
+        constructor(ctx, w) {
+            super(ctx, w);
+            this.ctx = ctx;
+            this.stateManger = new StateManager();
+            this.stateManger.pushState(new Explore(ctx, w, 'rpg', this));
+        }
+        update = () => {
+            sManager.updateStack();
+        }
+        endLevel(ctx) {
+            this.stateManger.pushState(new Cutscene("id", 800, 600, this.ctx, '2', this));
+        }
+    }
+}
