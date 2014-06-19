99ca652f56e12cf8c983726ab127fb87d6882164
PLyn <plyn10@stu.ncu.edu.jm>
2014-06-19 09:22:51 -0500

    changed folder name to remove space

---
+++ RoW/Tests/test.test.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="unitTest.ts" /> 
+class test{
+	Select(): void{
+		Assert.AreEqual(0, 1, "values must be equal"); 
+	}
+}
+
+var TestBase = { 
+    RegisterTestMethod: function (a, b, c) { } 
+}; 
+ 
+TestBase.RegisterTestMethod(test.prototype.Select, "Select test", "Runs selection and stuff"); 
